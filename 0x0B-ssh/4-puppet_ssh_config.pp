086cdf47733d4302fefa289d2e36b4ab37e1718e
billhong6981 <bill.burton6981@gmail.com>
2019-08-07 16:34:12 +0000

    advanced

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 # puppet manifest file that modifies client configuration file
 # so that login server using key authentication and no password asked
-file_line { 'No_password':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
-  match  => '\s*PasswordAuthentication yes',
 }
 
-file_line { 'Key_file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
-  match  => '\s*IdentityFile **',
 }
