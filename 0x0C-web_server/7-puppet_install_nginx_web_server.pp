a960ae240b3c598d3c880a62c6070e886276130a
billhong6981 <bill.burton6981@gmail.com>
2019-08-17 02:47:20 +0000

    advanced task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,23 @@
-# puppet manifest to instal nginx
-package { 'nginx':
-	ensure => installed,
-}
+# Automated by Puppet manifest to install web server nginx
+# and reconfiguration
 
-file_line { 'Add redirection, 301':
-	  ensure => 'present',
-	  path => '/etc/nginx/sites-available/default',
-	  after => 'listen 80 default_server;',
-	  line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4;',
+exec { 'update':
+  command => '/usr/bin/apt-get update',
 }
-
-file { '/var/www/html/index.html':
-     content => 'Holberton School',
+-> package { 'nginx':
+  ensure  => installed,
+  require => Exec['update'],
 }
-
-service { 'nginx':
-	ensure => running,
-	require => Package['nginx'],
+-> file_line { 'Add redirection, 301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4;',
+}
+-> file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+-> service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
