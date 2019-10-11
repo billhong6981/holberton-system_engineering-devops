a03d861834a49666a936caf1074e2c37c83cde69
billhong6981 <bill.burton6981@gmail.com>
2019-10-11 17:30:18 +0000

    puppet-lint style

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # puppet manifest to automate changing ULIMIT size of open files in nginx
 
 exec { 'changeMe':
-     path    => ['/bin/'],
-     command => 'sed -i "s/-n 15/-n 1000/" /etc/default/nginx',
-     notify  => Service['nginx']
+  path    => ['/bin/'],
+  command => 'sed -i "s/-n 15/-n 1000/" /etc/default/nginx',
+  notify  => Service['nginx']
 }
 
 service { 'nginx':
-	ensure	=> running,
-	enable	=> true
+  ensure => running,
+  enable => true
 }
